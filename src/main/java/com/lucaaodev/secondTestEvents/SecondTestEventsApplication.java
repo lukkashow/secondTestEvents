5fd558642b9b3a2c960f0a9c39bb0a36c8b6c3c5
Adryan Lucas Morais Ribeiro <me.lucasmorais@outlook.com>
2025-10-15 19:05:25 -0300

    Create Project

---
+++ src/main/java/com/lucaaodev/secondTestEvents/SecondTestEventsApplication.java
@@ -0,0 +1,13 @@
+package com.lucaaodev.secondTestEvents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondTestEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondTestEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucaaodev/secondTestEvents/SecondTestEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaaodev.secondTestEvents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondTestEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
